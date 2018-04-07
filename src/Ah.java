9bd1addb847d6e208623529723b76c124a26787a
vj613177 <vj613177@PC-2I-12.iutnice.unice.fr>
2018-04-07 08:30:41 +0200

    add des fichiers

---
+++ src/Ah.java
@@ -0,0 +1,12 @@
+
+public class Ah {
+public String hi;
+
+public String getHi() {
+	return hi;
+}
+
+public void setHi(String hi) {
+	this.hi = hi;
+}
+}
---
+++ src/Trig.java
@@ -0,0 +1,78 @@
+/*JL Vanni Mars2018
+
+ * TD Java Triangle
+
+ * Objet class Atrig
+
+ */
+
+
+
+public class Trig {
+
+
+
+	
+
+	public static void  main (String[] args) {
+
+		
+
+		
+
+		//voici un triangle okay
+
+		Atrig okay = new Atrig("toto");
+
+		okay.ligne=4;
+
+		okay.colonne=5;
+
+		//okay.Afficher();
+		//Ah ahah=new Ah();
+
+		//Ah.hi=("ici");
+		//.SetHi("ici");
+		//ahah.setHi("ici");
+		//System.out.print(ahah.getHi());
+		
+
+		
+
+		// voici un triangle oki
+
+		Atrig oki = new Atrig("tata");
+
+		oki.ligne=3;
+
+		oki.colonne=5;
+
+		oki.Afficher();
+
+		
+
+		
+
+/*		
+
+		System.out.println(okay.getNom());
+
+		System.out.println(okay.getLigne());
+
+		System.out.println(okay.getColonne());
+
+		
+
+*/		
+
+	}
+
+	
+
+	
+
+	
+
+	
+
+}
