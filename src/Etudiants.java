4be15285f32c74de91a904200cb431133ce74886
vj613177 <vj613177@PC-2I-10.iutnice.unice.fr>
2018-04-07 10:27:08 +0200

    ajout des etudiants

---
+++ src/Etudiants.java
@@ -0,0 +1,47 @@
+public class Etudiants {
+	
+public  static int id;
+public  static String nom;
+public  static String prenom;
+public  static String age;
+public  static String sexe;
+
+
+public static int getId() {
+	return id;
+}
+public static void setId(int id) {
+	Etudiants.id = id;
+}
+public static String getNom() {
+	return nom;
+}
+public static void setNom(String nom) {
+	Etudiants.nom = nom;
+}
+public static String getPrenom() {
+	return prenom;
+}
+public static void setPrenom(String prenom) {
+	Etudiants.prenom = prenom;
+}
+public static String getAge() {
+	return age;
+}
+public static void setAge(String age) {
+	Etudiants.age = age;
+}
+public static String getSexe() {
+	return sexe;
+}
+public static void setSexe(String sexe) {
+	Etudiants.sexe = sexe;
+}
+
+
+
+
+
+
+
+}
---
+++ src/Getudiants.java
@@ -0,0 +1,192 @@
+import java.util.*;
+
+
+//private static Scanner scan;
+
+//public Scanner sc = new Scanner(System.in);
+
+
+
+public class Getudiants {
+
+
+
+
+
+	public static void gestion() {
+		int indxe=0;
+		int sortie=1;
+		int e;
+		ArrayList<Etudiants> malistetud = new ArrayList<Etudiants>();
+		Scanner scan = new Scanner(System.in);
+
+		
+		//ettdt[] = null;
+		
+		while (sortie==1) {
+			
+		System.out.println("###############################");
+		System.out.println("Menu General");
+		System.out.println("###############################");
+
+	    
+		System.out.println("1/Ajouter un etudiant ");
+		System.out.println("2/Supprimer un etudiant ");
+		System.out.println("3/Modifier un etudiant ");
+		System.out.println("4/Afficher un etudiant ");
+		System.out.println("5/Afficher tous les etudiants ");
+		System.out.println("8/Quit ");
+		System.out.println("--------------------------------");
+		Scanner scanmenu = new Scanner(System.in);
+		Etudiants ettd=new Etudiants();
+		//i=scanmenu.next();
+		switch(scanmenu.next())
+        	{
+            case ("1"):
+                //ajoutedun();
+            	System.out.println("################");
+            	System.out.println("#Ajout#");
+            	System.out.println("################");
+            
+            	
+    			//Etudiants ettd=new Etudiants();
+            	//ettd[indxe]=new Etudiants();
+            	//malistetud.add(Etudiants);
+    			ettd.setId(indxe);
+    			System.out.println("Le Nom:");
+    			ettd.setNom(scan.next());
+    			
+    			malistetud.add(ettd);
+    			indxe++;
+            	break;
+            	
+            case ("2"):
+                //supprimedun();
+            	
+            	System.out.println("################");
+            	System.out.println("#Suppression#");
+            	System.out.println("################");
+            
+            	
+    			e=Integer.parseInt(scan.next()); 
+            	
+    			System.out.println("ID:");
+            	malistetud.remove(e);
+    			
+    			//indxe++;
+            	break;
+            	
+            case ("3"):
+                //Modifdun();
+            	
+            	System.out.println("################");
+            	System.out.println("#Modification#");
+            	System.out.println("################");
+            	System.out.println("ID:");
+            	
+    			e=Integer.parseInt(scan.next()); 
+            	
+    			
+            	malistetud.remove(e);
+            	
+            	
+    			
+            	//ettd[indxe]=new Etudiants();
+            	//malistetud.add(Etudiants);
+    			ettd.setId(indxe);
+    			System.out.println("Le Nom:");
+    			ettd.setNom(scan.next());
+    			
+    			malistetud.add(ettd);
+    			indxe++;
+    			
+    			//indxe++;
+            	break;
+            	
+            	
+            case ("4"):
+            	// afficher tous
+            	
+           	System.out.println("################");
+        	System.out.println("#Afficher un#");
+        	System.out.println("################");
+            	//malistaffich();
+            	//System.out.println(malistetud);
+        	System.out.println("ID:");
+        	
+			e=Integer.parseInt(scan.next()); 
+        	
+        	
+            
+            	
+            	//System.out.println(i+"#"+malistetud.get(i).getNom());
+            	System.out.println(e+"#"+malistetud.get(e));
+            	
+            
+            //System.out.println("------------------------");
+            	break;
+            	
+            	
+            case ("5"):
+            	// afficher tous
+            	int i;
+           	System.out.println("################");
+        	System.out.println("#Afficher tous#");
+        	System.out.println("################");
+            	//malistaffich();
+            	//System.out.println(malistetud);
+        	System.out.println(malistetud.size()+" Etudiants");
+        	System.out.println("------------------------");
+            
+        	for(i=0;i<malistetud.size();i++) {
+            	//System.out.println(i+"#"+malistetud.get(i).getNom());
+            	System.out.println(i+"#"+malistetud.get(i));
+        	}
+            
+            //System.out.println("------------------------");
+            	break;
+            case ("8"):
+            	sortie=0;
+            	break;
+            /*case 'valeur2':
+                action2;
+            break;
+            case 'valeur3':'valeur4':
+                action3;
+            break;
+            default:
+                action4;
+            break;*/
+        	}
+		//return scanmenu.next();
+		}
+		
+	}
+
+
+
+
+
+
+
+
+
+
+public static void main(String[] args) {
+		
+		
+		
+
+		
+		gestion();
+	
+		
+		
+	}
+
+
+
+
+
+
+}
